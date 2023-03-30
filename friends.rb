42ede27fa2d603c3f0bf9deab0e5ee919b1174b7
Anna Wiley <awiley33@gmail.com>
2023-03-29 19:22:24 -0500

    Use array method

--- friends.rb
+++ friends.rb
@@ -1,2 +1,6 @@
 arrays = ["Andi", "Seth", "Jeb", "Todd", "Sam"]
 puts arrays[0]
+visits = ["Mexico", "Italy", "France", "Venice", "Iceland"]
+visits.each do |visit|
+puts "I can't wait to visit #{visit}"
+end
